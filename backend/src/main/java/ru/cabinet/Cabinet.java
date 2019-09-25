57b9ceba2b34787fa28e604882ce0c5e04dc08e3
Gumpylon Arsalan <nosi95@gmail.com>
2019-09-25 09:45:19 +0300

    Initial commit

---
+++ backend/src/main/java/ru/cabinet/Cabinet.java
@@ -0,0 +1,12 @@
+package ru.cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cabinet {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cabinet.class, args);
+	}
+}
---
+++ backend/src/main/java/ru/cabinet/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package ru.cabinet.repository;
+
+
+import ru.cabinet.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(@Param("lastname") String lastname);
+
+    List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
